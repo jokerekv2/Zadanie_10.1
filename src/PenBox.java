1ec99e76f9287551c240dfcff9aee608c469b30b
Dominik Mikolajczyk <jokerekv2@gmail.com>
2020-11-01 21:01:37 +0100

    poprawa rozwiązania

--- src/PenBox.java
+++ src/PenBox.java
@@ -1,27 +1,26 @@
 public class PenBox {
     private static final int MAX_PENS_IN_BOX = 3;
-    private int numberOfPens = 0;
 
     private PensFactory pensFactory = new PensFactory();
     private Pen[] pens = new Pen[MAX_PENS_IN_BOX];
 
     public void fillTheBox() {
         pens[0] = pensFactory.createPen();
-        numberOfPens++;
-        for (int i = numberOfPens; i < MAX_PENS_IN_BOX; i++) {
+        for (int i = 1; i < MAX_PENS_IN_BOX;) {
             Pen pen = pensFactory.createPen();
             if (!compare(pen)) {
                 pens[i] = pen;
-                numberOfPens++;
+                i++;
             } else {
                 System.out.println("Duplikat! ");
-                i--;
             }
         }
     }
 
     public boolean compare(Pen pen) {
-        for (int i = 0; i < numberOfPens; i++) {
+        for (int i = 0; i < pens.length; i++) {
+            if (pens[i] == null)
+                return false;
             boolean duplicate = pens[i].equals(pen);
             if (duplicate)
                 return true;
